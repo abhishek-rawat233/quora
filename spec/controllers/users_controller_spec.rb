cfff0fe82f2fe6ca22641f024abb11d97f709692
abhishek-rawat233 <abhishek.rawat@vinsol.com>
2020-03-20 18:53:55 +0530

    eager load tested

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,22 @@ describe UsersController, type: :controller do
     session[:api_token] = subject.api_token
   end
 
-  
+  describe 'get profile image' do
+    context 'method return value' do
+      it do
+        user = double(User)
+        allow(user).to receive(:get_profile_image) { 'image' }
+        expect(user.get_profile_image).to eq('image')
+      end
+    end
+  end
+
+  describe 'question_ids' do
+    it do
+      allow(subject).to receive(:question_ids) {[1,2,3,4]}
+      expect(subject.question_ids).to eq([1,2,3,4])
+    end
+  end
   describe 'GET user#EDIT profile' do
     before do
       get :edit, params: { id: subject.id }
@@ -15,6 +30,7 @@ describe UsersController, type: :controller do
 
     it "renders edit profile" do
       expect(response).to render_template(:edit)
+      expect(assigns(:topics)).to eq Topic.all
     end
   end
 
@@ -24,6 +40,8 @@ describe UsersController, type: :controller do
     end
     it "renders home page" do
       expect(response).to render_template("home")
+      expect(assigns(:questions)).to eq Question.all.order(updated_at: :desc)
+      expect(assigns(:user_following_ids)).to eq subject.following_ids
     end
   end
 
@@ -38,12 +56,11 @@ describe UsersController, type: :controller do
 
   describe 'GET user#INDEX' do
     before do
-      # question = double("question", title: 'first one', content: 'in a while', question_type: 'published')
-    
       get 'index'
     end
     it 'renders index' do
       expect(response.header['Content-Type']).to include 'application/json'
+      expect(assigns(:user).association(:questions)).to be_loaded
     end
   end
 
@@ -66,21 +83,6 @@ describe UsersController, type: :controller do
 
     it "flashes notice" do
       expect(flash[:notice]).to eq('successfully uploaded')
-    end
-  end
-
-  describe 'get profile image' do
-    it do
-      user = double(User)
-      allow(user).to receive(:get_profile_image) { 'image' }
-      expect(user.get_profile_image).to eq('image')
-    end
-  end
-
-  describe 'question_ids' do
-    it do
-      allow(subject).to receive(:question_ids) {[1,2,3,4]}
-      expect(subject.question_ids).to eq([1,2,3,4])
     end
   end
 end
